dfad5f97032055cb140356f495df6d08e14a5d0f
siyabonga mahlalela <siyabonga.mahlalela@imaginescholar.org>
2023-02-07 00:02:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Siyabonga Mahlalela
+
+namespace Character_Frequencies_in__a_Message
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{			
+			char cLetter = 'A';
+			bool isCorrect;
+			char[] cLetters = new char[26];
+			int iNum;
+			for (int i = 0; i < 26; i++)
+			{
+				cLetters[i] = cLetter;
+				cLetter++;// Adding the characters from A to character 26.
+			}
+
+			do
+			{
+				Console.WriteLine("Enter a message: ");
+				string sMessage = Console.ReadLine().ToUpper();
+
+				isCorrect = int.TryParse(sMessage, out iNum);//Checks if user input can be converted from a string to a number
+                if (isCorrect == false)// If the user input cannot be successfully converted to a string, the condition is true
+                {
+                    Console.WriteLine("\nLetter Frequencies: ");
+                    Console.WriteLine();
+                    int cLength = cLetters.Length;
+                    Graph(sMessage, cLetters);
+                }
+                else if (isCorrect)
+                {
+                    Console.WriteLine("\n\nInvalid input. Press any key to start over.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+				 
+            } while (isCorrect);
+
+			Console.WriteLine("\nPress any key to exit..."); 
+			Console.ReadKey();
+		}
+		static void Graph(string _sMessage, char[] _cLetters)
+		{
+			for (int i = 0; i < _cLetters.Length; i++)//The loop is responsible for the 26 rows.
+			{
+				Console.Write(_cLetters[i]);//Printing the letter of the row
+				for (int j = 0; j < _sMessage.Length; j++)// The loop is responsible for the columns and printing the stars depending on use of the letters.
+				{
+					if (_cLetters[i] == _sMessage[j])
+					{
+						Print(_sMessage, _sMessage[j]); 
+					}
+					else
+					{
+						Console.Write("");
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+		static double Sum(string _sMessage, char _cLetter)
+		{
+			int dSum = 0;
+			for (int i = 0; i < _sMessage.Length; i++)
+			{
+				if (_sMessage[i] == _cLetter)
+				{
+					dSum += 1;
+				}
+			}
+			return dSum; // returns dSum which is a total of how much a letter has been used.
+		}
+		static void Print(string _sMessage, char _cLetter)
+		{
+			double dSum = Sum(_sMessage, _cLetter);
+            if (dSum > 1)
+            {
+                dSum = (Math.Sqrt(dSum) / dSum);
+            }
+            for (int i = 0; i < dSum; i++)// Prints stars according to the sum
+			{
+				Console.Write(" *");
+			}
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Character_Frequencies_in_ a_Message")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Character_Frequencies_in_ a_Message")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c27fcb8-3d51-4002-ba87-2b8b4f1ef7cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
